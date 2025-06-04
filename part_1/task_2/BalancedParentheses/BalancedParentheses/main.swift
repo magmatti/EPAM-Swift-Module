258d3dd60b8fb3145ace633cdb27bec53d54aa21
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-06-04 15:42:18 +0200

    fix isBalancedParentheses function

--- part_1/task_2/BalancedParentheses/BalancedParentheses/main.swift
+++ part_1/task_2/BalancedParentheses/BalancedParentheses/main.swift
@@ -2,24 +2,19 @@ import Foundation
 
 public func isBalancedParentheses(input: String) -> Bool {
     var count = 0
-    var foundParentheses: Bool = false
-    
+
     for char in input {
-        
         if char == "(" {
             count += 1
-            foundParentheses = true
         } else if char == ")" {
             count -= 1
-            foundParentheses = true
             if count < 0 {
                 return false
             }
         }
     }
-    
-    // if count equals zero it means all parentheses are closed
-    return foundParentheses && count == 0
+
+    return count == 0
 }
 
 // tests
